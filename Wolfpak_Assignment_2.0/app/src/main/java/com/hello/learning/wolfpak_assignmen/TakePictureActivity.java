8d7f81651064a5bab8fd67447a561a2787c5de8f
Test <nikhil.jain0987@gmail.com>
2015-06-20 23:44:56 -0400

    Initial commit

---
+++ Wolfpak_Assignment_2.0/app/src/main/java/com/hello/learning/wolfpak_assignmen/TakePictureActivity.java
@@ -0,0 +1,30 @@
+package com.hello.learning.wolfpak_assignmen;
+
+import com.hello.learning.wolfpak_assignmen.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+/**
+ * take the picture, etc
+ * edit this later to say something useful
+ *
+ * @author Nikhil Jain
+ */
+public class TakePictureActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_picture);
+
+        
+
+    }
+}
