68c835c84f45056dfd1bcf0f19e02d5b0eb4d161
Test <nikhil.jain0987@gmail.com>
2015-06-21 00:02:46 -0400

    Added camera

--- Wolfpak_Assignment_2.0/app/src/main/java/com/hello/learning/wolfpak_assignmen/TakePictureActivity.java
+++ Wolfpak_Assignment_2.0/app/src/main/java/com/hello/learning/wolfpak_assignmen/TakePictureActivity.java
@@ -4,11 +4,15 @@ import com.hello.learning.wolfpak_assignmen.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 
 /**
@@ -19,12 +23,32 @@ import android.view.View;
  */
 public class TakePictureActivity extends Activity {
 
+    final int CAMERA_REQUEST_CODE = 0;
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
 
-        
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        // Launch the camera
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST_CODE) {
+
+            Bitmap pictureBMP = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(pictureBMP);
+
+        }
 
     }
 }
